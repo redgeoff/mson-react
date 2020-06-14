3414d6c0b9a10b5fefb46eef5c34de8263235eb4
Geoff Cox <redgeoff@gmail.com>
2020-06-14 09:49:30 -0700

    feat(icon-buttons): optimized tooltips (#190)
    * feat(icon-buttons): optimized tooltips
    
    * refactor(tooltip-icon-button): better name
    
    * refactor(tooltip-icon-button): Fragment not needed

---
+++ src/tooltip-icon-button.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import Icon from './icon';
+import Tooltip from '@material-ui/core/Tooltip';
+
+const TooltipIconButton = (props) => {
+  const { onClick, icon, tooltipTitle } = props;
+
+  const [hovered, setHovered] = useState(false);
+
+  // NOTE: using Tooltips when we have 100 more items leads to a significant latency. We sidestep
+  // this by only loading the tooltip on the initial mouse over.
+
+  const button = (
+    <IconButton onClick={onClick} onMouseEnter={() => setHovered(true)}>
+      <Icon icon={icon} />
+    </IconButton>
+  );
+
+  if (hovered) {
+    return <Tooltip title={tooltipTitle}>{button}</Tooltip>;
+  } else {
+    return button;
+  }
+};
+
+export default TooltipIconButton;
