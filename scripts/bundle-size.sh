bc31b482aa708ae9a482ac7069aae13099c79b8b
Geoff Cox <redgeoff@gmail.com>
2020-12-20 21:17:36 -0800

    feat(commitizen): formatted commit messages (#236)
    * feat(commitizen): formatted commit messages
    
    * chore(bundle-size): bump as deps larger

--- scripts/bundle-size.sh
+++ scripts/bundle-size.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-mainMaxBytes=395000
-chunkMaxBytes=1300000
+mainMaxBytes=400000
+chunkMaxBytes=1000000
 
 if [ $(wc -c < build/static/js/main.*.js) -gt ${mainMaxBytes} ]; then
   echo 'Error: main bundle too large!'
   exit 1
 fi
 
-if [ $(wc -c < build/static/js/1.*.chunk.js) -gt ${chunkMaxBytes} ]; then
+if [ $(wc -c < build/static/js/2.*.chunk.js) -gt ${chunkMaxBytes} ]; then
   echo 'Error: chunk bundle too large!'
   exit 1
 fi
