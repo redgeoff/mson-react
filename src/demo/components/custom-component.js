c44050f2cbb1672f041fe437e6fda360792dc415
Geoff Cox <redgeoff@gmail.com>
2021-08-07 14:03:06 -0700

    feat(custom-component): functional form

--- src/demo/components/custom-component.js
+++ src/demo/components/custom-component.js
@@ -4,27 +4,57 @@ import Form from 'mson/lib/form';
 import TextField from 'mson/lib/fields/text-field';
 import attach from '../../attach';
 import Typography from '@material-ui/core/Typography';
+// import MSONComponent from 'mson/lib/component/mson-component';
 
-class CustomComponent extends UIComponent {
-  _className = 'CustomComponent';
+// class CustomComponent extends UIComponent {
+//   _className = 'CustomComponent';
 
-  _create(props) {
-    super._create(props);
+//   _create(props) {
+//     super._create(props);
 
-    this.set({
-      schema: new Form({
-        fields: [
-          new TextField({
-            name: 'name',
-          }),
-          new TextField({
-            name: 'label',
-          }),
-        ],
+//     this.set({
+//       schema: new Form({
+//         fields: [
+//           new TextField({
+//             name: 'name',
+//           }),
+//           new TextField({
+//             name: 'label',
+//           }),
+//         ],
+//       }),
+//     });
+//   }
+// }
+
+// const CustomComponent = new MSONComponent({
+//   name: 'CustomComponent',
+//   schema: new Form({
+//     fields: [
+//       new TextField({
+//         name: 'name',
+//       }),
+//       new TextField({
+//         name: 'label',
+//       }),
+//     ],
+//   })
+// })
+
+const CustomComponent = () => ({
+  component: UIComponent,
+  name: 'CustomComponent',
+  schema: new Form({
+    fields: [
+      new TextField({
+        name: 'name',
       }),
-    });
-  }
-}
+      new TextField({
+        name: 'label',
+      }),
+    ],
+  }),
+});
 
 let CustomComponentUI = (props) => {
   const { name, label } = props;
