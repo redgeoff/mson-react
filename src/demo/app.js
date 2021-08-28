02e8e252f7c01ce4b2d4837d64d1420735de47d4
Geoff Cox <redgeoff@gmail.com>
2021-08-28 11:50:17 -0700

    feat(demo): use register helper

--- src/demo/app.js
+++ src/demo/app.js
@@ -1,4 +1,5 @@
 import compiler from 'mson/lib/compiler';
+import register from 'mson/lib/compiler/register';
 import components from './components';
 import globals from 'mson/lib/globals';
 import uiComponents from '../components';
@@ -23,27 +24,24 @@ import { CustomComponentAttach } from './components/custom-component-attach';
 globals.set({ reCAPTCHASiteKey: '6LdIbGMUAAAAAJnipR9t-SnWzCbn0ZX2myXBIauh' });
 
 // Register optional core components
-compiler.registerComponent('FieldEditorForm', FieldEditorForm);
+register('FieldEditorForm', FieldEditorForm);
 uiComponents.FieldEditorForm = FieldEditorFormUI;
-compiler.registerComponent('FormEditor', FormEditor);
+register('FormEditor', FormEditor);
 uiComponents.FormEditor = FormEditorUI;
-compiler.registerComponent('FormBuilder', FormBuilder);
+register('FormBuilder', FormBuilder);
 
 // You can register custom components with the backend and frontend separately
-compiler.registerComponent('CustomComponent', CustomComponent);
+register('CustomComponent', CustomComponent);
 uiComponents.CustomComponent = ReactCustomComponent;
-compiler.registerComponent('CustomComponentJS', CustomComponentJS);
+register('CustomComponentJS', CustomComponentJS);
 uiComponents.CustomComponentJS = ReactCustomComponentJS;
 
 // You can also register UIComponents that bundle the frontend and backend together
-compiler.registerComponent('CustomComponentNoProps', CustomComponentNoProps);
-compiler.registerComponent('CustomComponentAttach', CustomComponentAttach);
+register('CustomComponentNoProps', CustomComponentNoProps);
+register('CustomComponentAttach', CustomComponentAttach);
 
 // Register all the components
-for (let name in components) {
-  let component = components[name];
-  compiler.registerComponent(component.name, component);
-}
+Object.keys(components).forEach((name) => register(components[name]));
 
 // Instantiate the app
 const app = compiler.newComponent({
