fdc7a947b2411f7a5e6bfe7174ffebb86d1cbfdc
Geoff Cox <redgeoff@gmail.com>
2021-09-19 14:39:49 -0700

    refactor(component): use hooks

--- src/component.js
+++ src/component.js
@@ -1,58 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import InnerComponent from './inner-component';
 import compiler from 'mson/lib/compiler';
-import attach from './attach';
+import useComponent from './use-component';
 
-export class Component extends React.PureComponent {
-  state = { component: null };
+export default function Component(props) {
+  const { component, definition, ...childProps } = props;
+  const [compiledComponent, setCompiledComponent] = useState(null);
+  const { hidden } = useComponent(component, ['hidden']);
 
-  createComponent() {
-    const { definition } = this.props;
-    this.setState({ component: compiler.newComponent(definition) });
-  }
+  // Should we use the component generated from the definition?
+  const comp = component ? component : compiledComponent;
 
-  componentDidMount() {
-    if (this.props.definition) {
-      this.createComponent();
+  // Compile the component when the definition changes
+  useEffect(() => {
+    if (definition) {
+      setCompiledComponent(compiler.newComponent(definition));
     }
-  }
+  }, [definition]);
 
-  componentWillUnmount() {
-    const { component } = this.state;
+  useEffect(() => {
+    return () => {
+      // Unmount
+      // Did we use a definition to define a component in the state?
+      if (comp) {
+        // Fire the unmount before we destroy the component so that the event is not lost
+        comp.emitChange('unmount');
 
-    // Did we use a definition to define a component in the state?
-    if (component) {
-      // Fire the unmount before we destroy the component so that the event is not lost
-      component.emitChange('unmount');
+        // Remove all listeners to prevent listener leaks
+        comp.destroy();
+      }
+    };
+  }, []);
 
-      // Remove all listeners to prevent listener leaks
-      this.state.component.destroy();
-    }
-  }
-
-  componentDidUpdate(prevProps) {
-    // Is the definition changing?
-    if (this.props.definition !== prevProps.definition) {
-      this.createComponent();
-    }
-  }
-
-  render() {
-    const { definition, component, ...childProps } = this.props;
-    let comp = component;
-
-    // Should we use the component generated from the definition?
-    if (!component) {
-      comp = this.state.component;
-    }
-
-    // Note: the component can be falsy, e.g. it has not yet been defined
-    if (comp && comp.get('hidden')) {
-      return null;
-    } else {
-      return <InnerComponent component={comp} {...childProps} />;
-    }
+  // Note: the component can be falsy, e.g. it has not yet been defined
+  if (comp && hidden) {
+    return null;
+  } else {
+    return <InnerComponent component={comp} {...childProps} />;
   }
 }
-
-export default attach(['hidden'])(Component);
